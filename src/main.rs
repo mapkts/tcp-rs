5f055a0040e113b8f292470ff74a94a0ed515dec
Aaron Taner <mapkts@gmail.com>
2021-05-10 23:10:09 +0800

    feat: accept syn packet

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ struct Quad {
 
 fn main() -> io::Result<()> {
 	// TCP connnections.
-	let mut connections: HashMap<Quad, tcp::State> = Default::default();
+	let mut connections: HashMap<Quad, tcp::Connection> = Default::default();
 
 	// Set up tun inteface.
 	let mut config = tun::Configuration::default();
@@ -26,7 +26,7 @@ fn main() -> io::Result<()> {
 
 	#[cfg(target_os = "linux")]
 	config.platform(|config| {
-		config.packet_information(true);
+		config.packet_information(false);
 	});
 	let mut dev = tun::create(&config).unwrap();
 
@@ -34,15 +34,14 @@ fn main() -> io::Result<()> {
 	let mut buf = [0u8; 1054];
 	loop {
 		let nbytes = dev.read(&mut buf)?;
-		let _tun_flags = u16::from_be_bytes([buf[0], buf[1]]);
-		let tun_proto = u16::from_be_bytes([buf[2], buf[3]]);
+		// let _tun_flags = u16::from_be_bytes([buf[0], buf[1]]);
+		// let tun_proto = u16::from_be_bytes([buf[2], buf[3]]);
+		// if tun_proto != 0x0800 {
+		// 	// Only ipv4 packets.
+		// 	continue;
+		// }
 
-		if tun_proto != 0x0800 {
-			// Only ipv4 packets.
-			continue;
-		}
-
-		let ip_hdr_offset = 4;
+		let ip_hdr_offset = 0;
 		match etherparse::Ipv4HeaderSlice::from_slice(&buf[ip_hdr_offset..nbytes]) {
 			Ok(ip_h) => {
 				let src = ip_h.source_addr();
@@ -56,14 +55,31 @@ fn main() -> io::Result<()> {
 				let tcp_hdr_offset = ip_hdr_offset + ip_h.slice().len();
 				match etherparse::TcpHeaderSlice::from_slice(&buf[tcp_hdr_offset..nbytes]) {
 					Ok(tcp_h) => {
+						use std::collections::hash_map::Entry;
 						let data_offset = tcp_hdr_offset + tcp_h.slice().len();
-						connections
-							.entry(Quad {
-								src_ip: (src, tcp_h.source_port()),
-								dst_ip: (dst, tcp_h.destination_port()),
-							})
-							.or_default()
-							.on_packet(ip_h, tcp_h, &buf[data_offset..nbytes]);
+						match connections.entry(Quad {
+							src_ip: (src, tcp_h.source_port()),
+							dst_ip: (dst, tcp_h.destination_port()),
+						}) {
+							Entry::Occupied(mut conn) => {
+								conn.get_mut().on_packet(
+									&mut dev,
+									ip_h,
+									tcp_h,
+									&buf[data_offset..nbytes],
+								)?;
+							}
+							Entry::Vacant(e) => {
+								if let Some(conn) = tcp::Connection::accept(
+									&mut dev,
+									ip_h,
+									tcp_h,
+									&buf[data_offset..nbytes],
+								)? {
+									e.insert(conn);
+								}
+							}
+						}
 					}
 					Err(e) => {
 						eprintln!("ignored weird packet: {:?}", e);
--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -1,27 +1,155 @@
-/// TCP state.
-#[derive(Clone, Debug)]
-pub struct State {}
+use std::io;
+use std::io::prelude::*;
 
-impl Default for State {
-    fn default() -> Self {
-        State {}
-    }
+/// The TCP state machine.
+///
+/// RFC 793 S3.2
+pub enum State {
+    Closed,
+    Listen,
+    SynReceived,
+    // Established,
 }
 
-impl State {
-    pub fn on_packet<'a>(
-        &mut self,
-        iph: etherparse::Ipv4HeaderSlice,
+/// An end to end TCP connection.
+pub struct Connection {
+    state: State,
+    send: SendSequenceSpace,
+    recv: RecvSequenceSpace,
+}
+
+/// State of the Send Sequence Space (RFC 793 S3.2 Fig4)
+///
+/// ```text
+/// 1         2          3          4
+/// ----------|----------|----------|----------
+///        SND.UNA    SND.NXT    SND.UNA
+///                             +SND.WND
+///
+/// 1 - old sequence numbers which have been acknowledged
+/// 2 - sequence numbers of unacknowledged data
+/// 3 - sequence numbers allowed for new data transmission
+/// 4 - future sequence numbers which are not yet allowed
+/// ```
+struct SendSequenceSpace {
+    /// send unacknowledged
+    una: u32,
+    /// send next
+    nxt: u32,
+    /// send window
+    wnd: u16,
+    /// send urgent pointer,
+    up: bool,
+    /// segment sequence number used for last window update
+    wl1: usize,
+    /// segment acknowledgment number used for last window update
+    wl2: usize,
+    /// initial send sequence number
+    iss: u32,
+}
+
+/// State of the Receive Sequence Space (RFC 793 S3.2 Fig5)
+///
+/// ```text
+/// 1          2          3
+/// ----------|----------|----------
+///        RCV.NXT    RCV.NXT
+///                  +RCV.WND
+///
+/// 1 - old sequence numbers which have been acknowledged
+/// 2 - sequence numbers allowed for new reception
+/// 3 - future sequence numbers which are not yet allowed
+/// ```
+struct RecvSequenceSpace {
+    /// receive next
+    nxt: u32,
+    /// receive window
+    wnd: u16,
+    /// receive urgent pointer
+    up: bool,
+    /// initial receive sequence number
+    irs: u32,
+}
+
+impl Connection {
+    pub fn accept<'a>(
+        nic: &mut tun::platform::Device,
+        ip_h: etherparse::Ipv4HeaderSlice,
         tcp_h: etherparse::TcpHeaderSlice,
         data: &'a [u8],
-    ) {
-        eprintln!(
-            "{}:{} -> {}:{}  {}B transfered over TCP",
-            iph.source_addr(),
-            tcp_h.source_port(),
-            iph.destination_addr(),
-            tcp_h.destination_port(),
-            data.len(),
+    ) -> io::Result<Option<Self>> {
+        let mut buf = [0u8; 1500];
+        // match self.state {
+        //     State::Closed => {
+        //         // refuse any packets
+        //         return Ok(None);
+        //     }
+        if !tcp_h.syn() {
+            // only expected SYN packets
+            return Ok(None);
+        }
+
+        let iss = 0;
+        let mut conn = Connection {
+            state: State::SynReceived,
+            send: SendSequenceSpace {
+                iss,
+                una: iss,
+                nxt: iss + 1,
+                wnd: 10,
+                up: false,
+                wl1: 0,
+                wl2: 0,
+            },
+            recv: RecvSequenceSpace {
+                irs: tcp_h.sequence_number(),
+                wnd: tcp_h.window_size(),
+                nxt: tcp_h.sequence_number() + 1,
+                up: false,
+            },
+        };
+
+        // need to start establishing a connection
+        let mut syn_ack =
+            etherparse::TcpHeader::new(tcp_h.destination_port(), tcp_h.source_port(), 0, 10);
+        syn_ack.acknowledgment_number = conn.recv.nxt;
+        syn_ack.syn = true;
+        syn_ack.ack = true;
+
+        let mut ip = etherparse::Ipv4Header::new(
+            syn_ack.header_len(),
+            64,
+            etherparse::IpTrafficClass::Tcp,
+            ip_h.destination_addr().octets(),
+            ip_h.source_addr().octets(),
         );
+
+        // write out the headers
+        let unwritten = {
+            let mut unwritten = &mut buf[..];
+            ip.write(&mut unwritten).unwrap();
+            syn_ack.write(&mut unwritten).unwrap();
+            unwritten.len()
+        };
+        nic.write(&buf[..unwritten])?;
+        // eprintln!(
+        //     "{}:{} -> {}:{}  {}B transfered over TCP",
+        //     ip_h.source_addr(),
+        //     tcp_h.source_port(),
+        //     ip_h.destination_addr(),
+        //     tcp_h.destination_port(),
+        //     data.len(),
+        // );
+        Ok(Some(conn))
+    }
+
+    pub fn on_packet<'a>(
+        &mut self,
+        nic: &mut tun::platform::Device,
+        ip_h: etherparse::Ipv4HeaderSlice,
+        tcp_h: etherparse::TcpHeaderSlice,
+        data: &'a [u8],
+    ) -> io::Result<()> {
+        Ok(())
     }
 }
